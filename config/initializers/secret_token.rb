0f0833a043fa74f22cbd0ba0f564a28bd0cab7d4
Ren Simmons <rsimmo4@gmail.com>
2013-11-24 02:53:28 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Belay::Application.config.secret_key_base = 'fa621cf64031bc5c85256ce1a65d9780bc8d8e440e47b96a7f059c49df5697ad358f63eb3f55531a3349e3ae97bf803d9652deaf4d3190042bec55a3bcded0c5'
